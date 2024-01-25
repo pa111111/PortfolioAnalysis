09fead9fd631d8b74a23df9e03c5ab85925cca52
pavel <pavel.artamokhov@gmail.com>
2024-01-25 13:44:26 +0300

    add cache func

---
+++ Utils/Cache_utils.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from hashlib import md5
+from dogpile.cache import make_region
+
+cache_region = make_region().configure('dogpile.cache.memory', expiration_time=3600)
+
+
+def cache_it(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        cache_key = md5(str((func.__name__, args, kwargs)).encode('utf-8')).hexdigest()
+        cached_result = cache_region.get(cache_key)
+        if cached_result is not None:
+            return cached_result
+        result = func(*args, **kwargs)
+        cache_region.set(cache_key, result)
+        return result
+
+    return wrapper
